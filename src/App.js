53c69a037a5895cd1c228a076eceaa8e54d26dec
Marcos Ricardo <mricardoalves99@gmail.com>
2019-09-29 00:20:16 -0300

    Set up routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
+import Routes from "./routes";
 import Header from "./components/Header";
 import "./styles.css";
 
 import Main from "./pages/main";
 
+
 //Stateless Component
 const App = () => (
   <div className="App">
     <Header />
-    <Main/>
+    <Routes/>
   </div>
 );
 
